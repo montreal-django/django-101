b76fac288f0c53fe729f47d0ac62cc5f4ec20bed
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2018-03-04 19:51:07 -0500

    modified quoter view and author model to fetch quotes from outside API

--- django101/quoter/models.py
+++ django101/quoter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=150)
-    birthdate = models.DateField()
+    birthdate = models.DateField(null=True)
 
     def __str__(self):
         return self.name
--- django101/quoter/views.py
+++ django101/quoter/views.py
@@ -1,5 +1,9 @@
+import json
+import urllib
+import html
+from random import choice
 from django.views.generic import ListView
-from .models import Quote
+from .models import Author, Category, Quote
 
 
 class QuoteView(ListView):
@@ -7,4 +11,23 @@ class QuoteView(ListView):
     template_name = 'random_quote.html'
 
     def get_queryset(self):
-        return Quote.objects.order_by('?').first()
+        if choice([True, False]) is True:
+            return Quote.objects.order_by('?').first()
+
+        with urllib.request.urlopen("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand") as url:
+            data = json.loads(url.read().decode())
+
+            category, created = Category.objects.get_or_create(
+                name='Quote from quotesondesign.com'
+            )
+
+            author, created = Author.objects.get_or_create(
+                name=data[0]['title']
+            )
+
+            quote, created = Quote.objects.get_or_create(
+                quote=html.unescape(data[0]['content'].replace('<p>', '').replace('</p>', '')),
+                defaults={'author': author, 'category': category}
+            )
+
+            return quote
